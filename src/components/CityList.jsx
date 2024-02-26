32a46b9996472e02a5c8e6187a0e54fdd6ab4d65
Rahimah Ansari <rahimeansari7@gmail.com>
2024-02-26 19:20:18 +0430

    Implementing the Cities List

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,7 +1,29 @@
-const CityList = () => {
-  return (
-    <div>CityList</div>
-  )
-}
+import styles from './CityList.module.css';
+import Spinner from '../components/Spinner';
+import CityItem from './CityItem';
+import PropTypes from 'prop-types';
+import Message from './Message';
 
-export default CityList
+const CityList = ({ cities, isLoading }) => {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message='Add your first city by clicking on a city on the map' />
+    );
+
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
+};
+
+CityList.propTypes = {
+  cities: PropTypes.array.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+};
+
+export default CityList;
